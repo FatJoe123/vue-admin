af64812c80d25f02b527981004816e7f9012b6e6
Alan You <1058413893@qq.com>
2017-03-31 17:37:51 +0800

    Create Home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <div style="text-align:center;">
+      <img style="width: 55%;" src="" alt="">
+    </div>
+  </section>
+</template>
+<script>
+  export default {}
+
+</script>
+<style scoped>
+
+</style>
