93b46a598207568178b1e7db03104d1f49cb672d
Alan You <1058413893@qq.com>
2017-03-31 17:37:24 +0800

    Create 404.vue

---
+++ src/pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <p class="page-container">404 page not found</p>
+</template>
+<style scoped>
+  .page-container {
+    padding-top: 30px;
+    font-size: 20px;
+    text-align: center;
+    color: #333;
+  }
+
+</style>
