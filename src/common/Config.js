d047c169413a2560d6fcb12e692843df0925c72c
Alan You <1058413893@qq.com>
2017-03-31 17:30:20 +0800

    Create Config.js

---
+++ src/common/Config.js
@@ -0,0 +1,17 @@
+/*
+ * 配置文件
+ */
+let Config = {
+    //接口地址
+    host: '',
+    //上传图片接口路径
+    uploadPath: "",
+    //图片路径
+    photoPath: "",                         
+    //ajax异常处理
+    ajaxError: '网络异常,请稍后重试',
+    //登陆超时
+    loginTimeOut: "登陆超时,请重新登陆"
+}
+
+export default Config
